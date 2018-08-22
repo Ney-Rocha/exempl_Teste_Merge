c661c02eb60824e81ad54fec599bb521c9369fdc
Roger William Belas Cruz <1801183@SALAS.AULAS>
2018-08-22 11:02:45 -0300

    Python tabuada

--- conexão.py
+++ conexão.py
@@ -1,2 +1,17 @@
-def mensage()
+def mensage():
     print("Criado com Python!!")
+
+def tabuada():
+    n1=int(input("Digite o numero que deseja a tabuada: "))
+    n2=0
+    while (n2<=10):
+            res=n1*n2
+            n2=n2+1
+            print(res)
+    return res
+
+res=tabuada()
+print(res)
+
+       
+     
